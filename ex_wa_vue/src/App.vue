831ce59e87ecbed2f85e92ad331dbc58b5262b44
unknown <raphael.billard3@gmail.com>
2023-04-26 01:51:06 +0200

    commit tardif

--- ex_wa_vue/src/App.vue
+++ ex_wa_vue/src/App.vue
@@ -1,14 +1,99 @@
 <template>
-  <HomePage/>
+  <div class="texts">
+    <span>Test technique WEB-ARIO réalisé par Billard Raphaël</span>
+    <span>réalisé le 25/04/2023</span>
+  </div>
+
+  <div v-for="(bar, index) in progressBars" :key="index">
+      <ProgressBar
+      :title="bar.title"
+      :percentage="bar.percentage"
+      />
+
+    <div class="buttons1">
+      <button class="button" type="button" v-on:click="reset(index)">
+        RAZ
+      </button>
+      <button class="button" type="button" v-on:click="addFive(index)">
+        + 5%
+      </button>
+      <button class="button" type="button" v-on:click="addTen(index)">
+        + 10%
+      </button>
+      <button class="button" type="button" v-on:click="removeProgressBar(index)">
+       Supprimer
+      </button>
+    </div>
+      
+  </div>
+
+  <div class="buttons2">
+    <button class="button" type="button" v-on:click="reset()">
+      Remettre à zéro les compteurs
+    </button>
+    <button class="button" type="button" v-on:click="addFive()">
+      Ajouter 5% à tous
+    </button>
+    <button class="button" type="button" v-on:click="addTen()">
+      Ajouter 10% à tous
+    </button>
+    <button class="button" type="button" v-on:click="addProgressBar()">
+      Ajouter une barre de progression
+    </button>
+  </div>
 </template>
 
 <script>
-import HomePage from './components/HomePage.vue'
+import ProgressBar from './components/ProgressBar.vue';
 
 export default {
   name: 'App',
   components: {
-    HomePage
+    ProgressBar
+  },
+  props: {},
+  data: function() {
+    return {
+      progressBars: [
+          { title: 'Initialisation du test technique', percentage: 50},
+          { title: 'Avancement de la phase de développement', percentage: 25}
+        ]
+    }
+  },
+  methods: {
+    reset(index = null) {
+      if (index) {
+        this.progressBars[index].percentage = 0;
+      }else {
+        this.progressBars.map(progressBar => {
+          progressBar.percentage = 0;
+        });
+      }
+    },
+    addFive(index = null) {
+      if (index) {
+        (this.progressBars[index].percentage += 5) > 100 ? this.progressBars[index].percentage = 100 : this.progressBars[index].percentage += 5;
+      }else {
+        this.progressBars.map(progressBar => {
+          (progressBar.percentage += 5) > 100 ?  progressBar.percentage = 100 : progressBar.percentage += 5;
+        });
+      }
+    },
+    addTen(index = null) {
+      if (index) {
+        (this.progressBars[index].percentage += 10) > 100 ? this.progressBars[index].percentage = 100 : this.progressBars[index].percentage += 10;
+      }else {  
+        this.progressBars.map(progressBar => {
+          (progressBar.percentage += 10) > 100 ?  progressBar.percentage = 100 : progressBar.percentage += 10;
+        });
+      }
+    },
+    addProgressBar() {
+      this.progressBars.push({ title: 'Barre de progression n°' + (this.progressBars.length + 1) , percentage: 0});
+    },
+    removeProgressBar(index) {
+      this.progressBars.splice(index, 1);
+    }
   }
 }
 </script>
@@ -19,7 +104,46 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+body {
+  background-color: #242424;
+}
+
+.texts {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: white;
+  margin: 5em;
+  font-size: 0.8em;
+  font-weight: bold;
+}
+
+.buttons1 {
+  display: flex;
+  justify-content: center;
+  margin-top: 0.5em;
+  margin-bottom: 1em;
+
+}
+
+.buttons2 {
+  display: flex;
+  justify-content: center;
+  margin-top: 5em;
+}
+
+.button {
+  background-color: #373737 ;
+  border: none;
+  margin: 0.5em;
+  padding: 1em;
+  color: white;
+  font-size: 0.7em;
+}
+
+.button:hover {
+  cursor: pointer;
 }
 </style>
--- ex_wa_vue/src/components/ProgressBar.vue
+++ ex_wa_vue/src/components/ProgressBar.vue
@@ -1,6 +1,11 @@
 <template>
-<p>{{title}}</p>
-<progress id="progress-bar" max="100" :value="starting_percentage" :style="style()"> {{starting_percentage}}%</progress>
+    <div>
+        <span class="title">{{title}}</span>
+        <div class="group">
+            <div class="label">{{percentage}}%</div>
+            <progress max="100" :value="percentage" :style="style(percentage)"></progress>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -8,17 +13,43 @@ export default {
   name: 'ProgressBar',
   props: {
     title: String,
-    starting_percentage: Number,
-    color: String
+    percentage: Number,
   },
   methods: {
-    style() {
-        return 'background: ' + this.color
+    style(percentage) {
+        if (percentage >= 0 && percentage < 25) {
+            return 'accent-color: #7160E8; width: 50%; height: 2em';
+        } else if (percentage >= 25 && percentage < 50) {
+            return 'accent-color: #60ADE8; width: 50%; height: 2em';
+        } else if (percentage >= 50 && percentage < 75) {
+            return 'accent-color: #60E8B6; width: 50%; height: 2em';
+        }  else if (percentage >= 75) {
+            return 'accent-color: #30DB63; width: 50%; height: 2em';
+        }
     }
   }
 }
 </script>
 
 <style>
+.title{
+    font-size: 0.6em;
+    margin-bottom: 5px;
+    color: white;
+    }
+
+.group {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+}
+
+.label {
+    margin-right: 0.5em;
+    color: #747474;
+    font-size: 1em;
+}
+
 
 </style>
